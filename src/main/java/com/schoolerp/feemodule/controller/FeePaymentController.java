ee668eec8aca1533a65ad934ddbc890f1cc7fed4
AKSHAYBARMATE <100841949+AKSHAYBARMATE@users.noreply.github.com>
2025-10-01 00:30:45 +0530

    Update FeePaymentController.java

--- src/main/java/com/schoolerp/feemodule/controller/FeePaymentController.java
+++ src/main/java/com/schoolerp/feemodule/controller/FeePaymentController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/fee-structures-module")
+@RequestMapping("/sms/api/v1/fee-structures-module")
 public class FeePaymentController {
 
     @Autowired
